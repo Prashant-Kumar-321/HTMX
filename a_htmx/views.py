d616876336ac4917edf72c634de8fbbdd4bb0177
Prashant-kumar-321 <pk6176838@gmail.com>
2024-12-05 15:26:08 +0530

    Litent htmx request event and conditionally issue request with indicator

--- a_htmx/views.py
+++ a_htmx/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def htmx_view(request): 
     return render(request, 'index.html')
 
+counter = 1
 def users_detail(request): 
-    return HttpResponse('<div class="text-lg text-orange-700">This is response from server</div>')
+    global counter 
+    # users =[
+    #     'Prashant Kumar Gupta',
+    #     'Nishant Kumar Gupta', 
+    #     'Amrita Tiwari', 
+    #     'Ravi Gupta', 
+    #     'Pramod Ganjhu',
+    # ]
+    counter += 1; 
+
+    response = requests.get('https://jsonplaceholder.typicode.com/users')
+    data = response.json() 
+
+    return render(request, 'snippets/users.html', {'users': data, 'counter': counter})
+
